6b3ce1a6e22ded679f03f0cefc4f6f69fa423ee9
Jacob Klimenko <droidedjake@gmail.com>
2021-07-24 22:23:15 -0400

    commit tree dbd441570eb2d01ef6a1fd14d21e815e845c0189

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Your code here
+function saturdayFun(activity="roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+};
+
+const mondayWork = function(activity="go to the office") {
+    return `This Monday, I will ${activity}.`;
+};
+
+function wrapAdjective(visualFlair="*") {
+    return function(adj="special") {
+        return `You are ${visualFlair + adj + visualFlair}!`;
+    };
+};
+
+const Calculator = {
+    add: function() {return 1 + 3;},
+    subtract: function() {return 1 - 3;},
+    multiply: function() {return 1 * 3;},
+    divide: function() {return 10 / 5;}
+};
+
+function actionApplyer(initNum, funcs) {
+    if (funcs === [] || !!funcs === false) {
+        return initNum;
+    } else {
+        let base = initNum;
+        for (const func of funcs) {
+            base = func(base);
+        };
+        return base;
+    };
+};
